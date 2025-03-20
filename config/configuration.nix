726c151e08d047fc2cd23c3f85b21cd2028ecbc9
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-03-20 02:49:01 +0000

    mutter tripple buffering enable

--- config/configuration.nix
+++ config/configuration.nix
@@ -52,6 +52,7 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -89,8 +90,37 @@
     curl
     home-manager
     ntfs3g
-  ];
 
+  ];
+  nixpkgs.overlays = [
+    (final: prev: {
+      mutter = prev.mutter.overrideAttrs (oldAttrs: {
+        # GNOME dynamic triple buffering (huge performance improvement)
+        # See https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1441
+        src = final.fetchFromGitLab {
+          domain = "gitlab.gnome.org";
+          owner = "vanvugt";
+          repo = "mutter";
+          rev = "triple-buffering-v4-47";
+          hash = "sha256-6n5HSbocU8QDwuhBvhRuvkUE4NflUiUKE0QQ5DJEzwI=";
+        };
+
+        preConfigure =
+          let
+            gvdb = final.fetchFromGitLab {
+              domain = "gitlab.gnome.org";
+              owner = "GNOME";
+              repo = "gvdb";
+              rev = "2b42fc75f09dbe1cd1057580b5782b08f2dcb400";
+              hash = "sha256-CIdEwRbtxWCwgTb5HYHrixXi+G+qeE1APRaUeka3NWk=";
+            };
+          in
+          ''
+            cp -a "${gvdb}" ./subprojects/gvdb
+          '';
+      });
+    })
+  ];
   nix.gc = {
     automatic = true;
     dates = "weekly";
