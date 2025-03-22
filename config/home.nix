a8d66221fd333187349e11b81887f3fd8b771ed0
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-03-22 21:32:53 +0000

    2025-03-22 21:32 : id 33 -> /nix/store/7byban4lvrhbs55mmbqhkpbil1b1g1hd-home-manager-generation

--- config/home.nix
+++ config/home.nix
@@ -22,6 +22,7 @@
     fastfetch
     tree
     nix-prefetch-git
+    vencord
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
