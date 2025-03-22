9532f7294366b533c169414436619ed73a39059d
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-03-22 20:30:58 +0000

    2025-03-22 20:30 : id 28 -> /nix/store/r1xsq7abivl56b8h28j8vjyzg7w8h4hy-home-manager-generation

--- config/stylix/stylix.nix
+++ config/stylix/stylix.nix
@@ -7,8 +7,8 @@
     base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
     image = ../../background.jpg;
     polarity = "dark";
-    targets = {
-      ghostty.enable = true;
+    opacity = {
+      terminal = 0.95;
     };
   };
 }
