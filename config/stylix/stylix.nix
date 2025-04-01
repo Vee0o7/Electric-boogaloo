73b52b9cfca66c48089c227e07fe374572929c2b
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-04-01 16:26:08 +0100

    2025-04-01 16:26 : id 44 -> /nix/store/kx2n2q8sqpy9xfjhk37kqafqgm0brf9s-home-manager-generation

--- config/stylix/stylix.nix
+++ config/stylix/stylix.nix
@@ -4,7 +4,14 @@
   stylix = {
     autoEnable = true;
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-plateau.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-lakeside.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-cave.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-heath.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/darkviolet.yaml";
     image = ../../background.jpg;
     polarity = "dark";
     opacity = {
