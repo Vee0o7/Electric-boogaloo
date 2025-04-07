d75e29b1277b35d8101398a2166f541249a8f8f5
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-04-07 18:00:17 +0100

    2025-04-07 18:00 : id 53 -> /nix/store/prj5svm9yhp92035649078dp7yzf9s2p-home-manager-generation

--- config/home.nix
+++ config/home.nix
@@ -25,6 +25,7 @@
     protonup
     vesktop
     wine
+    devenv
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
