b60ec67c309c570a52644e208efc2df26c49c7a4
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-04-08 15:09:04 +0100

    2025-04-08 15:09 : id 79 -> /nix/store/qwlsk1l9rvp48sah4rj4r360c7p1srj2-home-manager-generation

--- config/home.nix
+++ config/home.nix
@@ -27,6 +27,7 @@
     wine
     devenv
     expect
+    ansifilter
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
