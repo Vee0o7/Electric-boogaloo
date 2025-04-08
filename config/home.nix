77f00937b1985861e5472c960067fda532034c10
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-04-08 15:07:23 +0100

    2025-04-08 15:07 : id 78 -> /nix/store/1q67agl97lgs1lx4zg50wrjdirqhkk5y-home-manager-generation

--- config/home.nix
+++ config/home.nix
@@ -27,7 +27,6 @@
     wine
     devenv
     expect
-    colorized-logs
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
