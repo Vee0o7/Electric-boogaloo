c76d5afc4e1a50427486d1f8403491b1803d71f6
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-04-08 23:15:00 +0100

    2025-04-08 23:14 : id 105 -> /nix/store/xvdwanr3mbp0nw769v7jfy0wdkvgm0k5-home-manager-generation

--- config/home.nix
+++ config/home.nix
@@ -28,6 +28,7 @@
     expect
     gimp
     python3
+    bottles
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
