56b0c1cc4a41d2be5fab10358645d3bf84e0a102
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-04-08 12:56:10 +0100

    2025-04-08 12:49 : id 74 -> /nix/store/1q67agl97lgs1lx4zg50wrjdirqhkk5y-home-manager-generation

--- sync.sh
+++ sync.sh
@@ -73,7 +73,7 @@ else
         echo "no changes to commit"
 fi
 echo "working..."
-unbuffer $command |& tee switch.log
+unbuffer -p $command |& tee switch.log
 gen=$($gencmd | $gengrep)
 git commit -am "$gen"
 echo Done.
