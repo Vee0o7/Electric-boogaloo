14b2f43856c55f4db1162d012f8d5f8dfb05d615
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-04-11 16:50:54 +0100

    49 current  2025-04-11 16:50:48  25.05.20250409.f675531  6.12.22                          *

--- config/stylix/stylix.nix
+++ config/stylix/stylix.nix
@@ -12,25 +12,25 @@ let
       phases = [ "buildPhase" ];
       buildPhase = "echo '${yaml}' | ${pkgs.yaml2json}/bin/yaml2json > $out";
   }));
-  # pywal16palette = image:
-  #   builtins.fromJSON (builtins.readFile (pkgs.stdenv.mkDerivation {
-  #     name = "bg2colors";
-  #     nativeBuildInputs = [
-  #       pkgs.imagemagick
-  #       pkgs.colorz
-  #     ];
-  #     phases = [ "preBuild" "buildPhase" ];
-  #     preBuild = ''
-  #       export HOME=$(mktemp -d)
-  #       export CONF_DIR=$(mktemp -d)
-  #       export CACHE_DIR=$(mktemp -d)
-  #     '';
-  #     buildPhase = ''
-  #       ${pkgs.pywal16}/bin/wal -i ${background} --backend colorz
-  #       cat $HOME/.cache/wal/colors.json | awk 'NR != 2' > $out
-  #     '';
-  # }));
-  # palette = (pywal16palette background).colors;
+  pywal16palette = image:
+    builtins.fromJSON (builtins.readFile (pkgs.stdenv.mkDerivation {
+      name = "bg2colors";
+      nativeBuildInputs = [
+        pkgs.imagemagick
+        pkgs.colorz
+      ];
+      phases = [ "preBuild" "buildPhase" ];
+      preBuild = ''
+        export HOME=$(mktemp -d)
+        export CONF_DIR=$(mktemp -d)
+        export CACHE_DIR=$(mktemp -d)
+      '';
+      buildPhase = ''
+        ${pkgs.pywal16}/bin/wal -i ${background} --backend colorz
+        cat $HOME/.cache/wal/colors.json | sed 's/.*wallpaper.*/ /' > $out
+      '';
+  }));
+  palette = (pywal16palette background).colors;
 in
 {
   fonts.fontconfig.enable = true;
@@ -39,24 +39,24 @@ in
     enable = true;
     autoEnable = true;
     # accent colors are done separately with a custom gnome-shell (if using gnome)
-    # base16Scheme = {
-    #   base00 = palette.color0;
-    #   base01 = palette.color0;
-    #   base02 = palette.color2;
-    #   base03 = palette.color3;
-    #   base04 = palette.color4;
-    #   base05 = palette.color15;
-    #   base06 = palette.color6;
-    #   base07 = palette.color7;
-    #   base08 = palette.color8;
-    #   base09 = palette.color9;
-    #   base0A = palette.color10;
-    #   base0B = palette.color11;
-    #   base0C = palette.color12;
-    #   base0D = palette.color13;
-    #   base0E = palette.color14;
-    #   base0F = palette.color15;
-    # };
+    base16Scheme = {
+      base00 = palette.color0;
+      base01 = palette.color0;
+      base02 = palette.color2;
+      base03 = palette.color3;
+      base04 = palette.color4;
+      base05 = palette.color15;
+      base06 = palette.color6;
+      base07 = palette.color7;
+      base08 = palette.color8;
+      base09 = palette.color9;
+      base0A = palette.color10;
+      base0B = palette.color11;
+      base0C = palette.color12;
+      base0D = palette.color13;
+      base0E = palette.color14;
+      base0F = palette.color15;
+    };
     image = background;
 
     cursor = {
