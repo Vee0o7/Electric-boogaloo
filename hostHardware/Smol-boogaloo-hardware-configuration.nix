54053bc6e992355f538799bb5ee3a239bd5d1a84
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-04-27 11:20:48 +0100

    94 current  2025-04-27 11:20:38  25.05.20250409.f675531  6.12.22                          *

--- hostHardware/Smol-boogaloo-hardware-configuration.nix
+++ hostHardware/Smol-boogaloo-hardware-configuration.nix
@@ -40,15 +40,23 @@
   services.power-profiles-daemon.enable = false;
   powerManagement.enable = true;
   services.thermald.enable = true;
-  services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.settings = {
-    battery = {
-       governor = "powersave";
-       turbo = "never";
-    };
-    charger = {
-       governor = "performance";
-       turbo = "auto";
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+
+      #Optional helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 60; 
+      STOP_CHARGE_THRESH_BAT0 = 80; 
     };
   };
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
