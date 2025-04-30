4a20005f32500de89511e40432f0019baac815f1
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-04-30 20:21:51 +0100

    101 current  2025-04-30 20:21:41  25.05.20250429.46e634b  6.12.25                          *

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "system flake";
+  
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+    stylix.url = "github:danth/stylix";
+  };
+  
+  outputs = {self, nixpkgs, home-manager, nixvim, stylix, nixpkgs-unstable, ...}:
+    let 
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      config = [
+          ./gnome/configuration.nix
+
+          ./config/configuration.nix
+          stylix.nixosModules.stylix
+          home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.viv = {imports = [./gnome/home.nix ./config/home.nix];};
+              home-manager.backupFileExtension = "hm-backup";
+              home-manager.extraSpecialArgs = {
+                inherit nixvim;
+              };
+            }
+      ];
+    in {
+    nixosConfigurations = {
+      Big-boogaloo = lib.nixosSystem {
+        inherit system;
+        modules = [ 
+          {networking.hostName = "Big-boogaloo";}
+          ./hostHardware/Big-boogaloo-hardware-configuration.nix
+        ] ++ config;
+      };
+      Smol-boogaloo = lib.nixosSystem {
+        inherit system;
+        modules = [ 
+          {networking.hostName = "Smol-boogaloo";}
+          ./hostHardware/Smol-boogaloo-hardware-configuration.nix
+        ] ++ config;
+      };
+    };
+  };
+}
