e6eae3fd3ef7de72f101ae738d224d54201ba28b
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-05-01 19:26:20 +0100

    122 current  2025-05-01 19:26:13  25.05.20250429.46e634b  6.12.25                          *

--- hypr/configuration.nix
+++ hypr/configuration.nix
@@ -1,6 +1,11 @@
 { pkgs, lib, config, inputs, ... }:
 {
-  services.xserver.displayManager.sddm = {
+  services.xserver.displayManager.gdm = {
     enable = true;
+  };
+  programs.hyprland = {
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    withUWSM = true;
   };
 }
--- hypr/home.nix
+++ hypr/home.nix
@@ -2,7 +2,7 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
-    systemd.enable = true;
+    systemd.enable = false;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     settings = {
