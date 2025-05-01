697640d22afee009eb68efb9aac946c0661e8f30
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-05-01 19:49:57 +0100

    125 current  2025-05-01 19:49:46  25.05.20250429.46e634b  6.12.25                          *

--- hypr/configuration.nix
+++ hypr/configuration.nix
@@ -1,7 +1,26 @@
 { pkgs, lib, config, inputs, ... }:
+let
+  tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+in
 {
-  services.xserver.displayManager.sddm = {
+  services.greetd = {
     enable = true;
+    settings = {
+      default_session = {
+        command = "${tuigreet} -- time --remember --cmd hyprland";
+        user = "viv";
+      };
+    };
+  };
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
   };
   programs.hyprland = {
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
