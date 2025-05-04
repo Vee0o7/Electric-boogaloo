1ad64ae9e78c89879abf797064da77777cfbb245
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-05-04 14:50:10 +0100

    144 current  2025-05-04 14:50:04  25.05.20250429.46e634b  6.12.25                          *

--- hostHardware/Smol-boogaloo/configuration.nix
+++ hostHardware/Smol-boogaloo/configuration.nix
@@ -6,29 +6,29 @@
     layout = "gb";
     variant = "";
   };
-  # # power saving settings
-  # services.power-profiles-daemon.enable = false;
+  # power saving settings
+  services.power-profiles-daemon.enable = false;
   # powerManagement.enable = true;
   # services.thermald.enable = true;
-  # services.tlp = {
-  #   enable = true;
-  #   settings = {
-  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-  #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
-  #
-  #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-  #     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-  #
-  #     CPU_MIN_PERF_ON_BAT = 0;
-  #     CPU_MAX_PERF_ON_BAT = 55;
-  #     CPU_MIN_PERF_ON_AC = 0;
-  #     CPU_MAX_PERF_ON_AC = 100;
-  #
-  #     #Optional helps save long term battery health
-  #     START_CHARGE_THRESH_BAT0 = 60;
-  #     STOP_CHARGE_THRESH_BAT0 = 80;
-  #   };
-  # };
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 55;
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+
+      #Optional helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+    };
+  };
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
