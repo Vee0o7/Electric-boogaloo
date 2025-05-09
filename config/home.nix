e420c4ee74046bc915f4e160c99dddb89879e433
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-05-09 13:08:28 +0100

    154 current  2025-05-09 13:08:26  25.05.20250429.46e634b  6.12.25                          *

--- config/home.nix
+++ config/home.nix
@@ -33,6 +33,7 @@
     vesktop
     fzf
     whatsie
+    libreoffice
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
