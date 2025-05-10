3d0311ffc70c7cc13eacc6314088ed12b18033fd
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-05-10 17:14:12 +0100

    156 current  2025-05-10 17:14:09  25.05.20250429.46e634b  6.12.25                          *

--- config/home.nix
+++ config/home.nix
@@ -35,6 +35,8 @@
     whatsie
     libreoffice
     ripgrep
+    imagemagick
+    fd
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
