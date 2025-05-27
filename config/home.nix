e92d4c83a619ac871d210b36c80d97436408eb97
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-05-27 23:26:22 +0100

    232 current  2025-05-27 23:26:13  25.11.20250525.62b852f  6.12.30                          *

--- config/home.nix
+++ config/home.nix
@@ -171,17 +171,14 @@
     autosuggestion.enable = true;
     oh-my-zsh = {
       enable = true;
-      plugins = ["direnv" "fzf" "git" "git-auto-fetch" "sudo" "copybuffer" "history"];
+      plugins = ["direnv" "fzf" "git" "thefuck" "git-auto-fetch" "copybuffer" "history"];
     };
-    initContent = let after = lib.mkOrder 1600 "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh"; before = lib.mkOrder 480 (''
-      if [[ -r "'' + ''$'' + ''{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$''+ ''{(%):-%n}.zsh" ]]; then
-      source "$''+ ''{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$'' + ''{(%):-%n}.zsh"
-      fi
+    initContent = let after = lib.mkOrder 1600 "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh"; before = lib.mkOrder 400 (''
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme'');
       in lib.mkMerge [before after];
   };
 
-  programs.thefuck.enable = true;
+  # programs.thefuck.enable = true;
   programs.dircolors = {
     enable = true;
     settings = {
