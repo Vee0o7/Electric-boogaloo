e87897bb977d2d06cc489aa7dac81e4ab1bc2767
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2025-06-25 00:14:45 +0100

    laptop

--- hostHardware/Smol-boogaloo/configuration.nix
+++ hostHardware/Smol-boogaloo/configuration.nix
@@ -65,6 +65,6 @@
     user = "viv";
   };
 
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
+  # systemd.services."getty@tty1".enable = false;
+  # systemd.services."autovt@tty1".enable = false;
 }
