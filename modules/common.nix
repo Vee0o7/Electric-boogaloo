d717067fb59bf15db5eba15f38c8d76c41b35159
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2026-03-30 15:39:50 +0100

    4           2026-03-30 15:39:46  26.05.20260321.6c9a78c   6.18.19  Unknown                 []              True

--- modules/common.nix
+++ modules/common.nix
@@ -69,8 +69,17 @@
 
     nix.settings = {
       experimental-features = ["nix-command" "flakes"];
-      substituters = ["https://hyprland.cachix.org" "https://noctalia.cachix.org" ];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://noctalia.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
 
     nixpkgs.config.packageOverrides = pkgs: {
