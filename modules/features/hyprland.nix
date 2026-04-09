5b9b07abb82028f94322ed5e7053674050bcb861
Vee0o7 <114813444+Vee0o7@users.noreply.github.com>
2026-04-10 00:59:44 +0100

    17          2026-04-10 00:59:41  26.05.20260328.8110df5   6.18.20  Unknown                 []              True

--- modules/features/hyprland.nix
+++ modules/features/hyprland.nix
@@ -84,7 +84,7 @@
       ];
 
       bindel = [
-        ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ & wpctl set-mute @DEFAULT_AUDIO_SINK@ 0"
         ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
         ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
         ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
